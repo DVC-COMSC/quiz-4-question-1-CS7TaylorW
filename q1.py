c9a68e0cdd689e174bb70d46ab415c36b2473aa6
CS7TaylorW <123520365+CS7TaylorW@users.noreply.github.com>
2023-03-07 04:47:10 +0000

    Quiz 4 Question 2

--- q1.py
+++ q1.py
@@ -2,6 +2,33 @@
 # ******************************
 # Make your Code
 # ******************************
+prev = 0
+prev2 = 0
+current = 0 
+cluster = 0 
+
+for i in range(10):
+    current = (int(input('Please enter a number')))
+    if i < 1:
+        prev = current
+    if i == 1:
+        if prev % 2 == 0 and current % 2 == 0:
+            prev2 = prev
+            prev = current
+            cluster = cluster + 1
+    if i >= 2:
+        if prev2 % 2 == 0 and prev % 2 == 0 and current % 2 == 0:
+            prev2 = prev
+            prev = current
+        elif prev % 2 == 0 and current % 2 == 0:
+            prev2 = prev
+            prev = current
+            cluster = cluster + 1
+        else:
+            prev2 = prev
+            prev = current
+
+print (cluster)
 
 
 # Requirement
